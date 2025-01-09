742cd5da889f5c082ebed01e25b75aa63be716a6
MLea1909 <126357233+MLea1909@users.noreply.github.com>
2025-01-09 14:28:56 +0100

    FormHomePage & open FormOverviewProjection without comm

--- Kino/view/FormHomePage.cs
+++ Kino/view/FormHomePage.cs
@@ -59,52 +59,34 @@ namespace Kino.view
             List<Movie> movies = movieService.GetMovies();
 
             for (int i = 0; i < movies.Count; i++)
-            {
-                //string resourceName = $"movie_{i + 1}.jpg"; // Dynamically construct the resource name
-                //PropertyInfo resourceProperty = resourceType.GetProperty(resourceName, BindingFlags.Static | BindingFlags.Public);
-
-                //if (resourceProperty != null)
+            { 
+                PictureBox pictureBox = new PictureBox
                 {
-                    //Image resourceImage = (Image)resourceProperty.GetValue(null);
+                    Size = new Size(pictureBoxWidth, pictureBoxHeight),
+                    Location = new Point(x, y),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Image = (Image)Properties.Resources.ResourceManager.GetObject($"movie_{i + 1}"),
+                    Tag = i + 1,
+                    BorderStyle = BorderStyle.FixedSingle
+                };
 
-                    PictureBox pictureBox = new PictureBox
-                    {
-                        Size = new Size(pictureBoxWidth, pictureBoxHeight),
-                        Location = new Point(x, y),
-                        SizeMode = PictureBoxSizeMode.Zoom,
-                        /*Image = resourceImage,*/
-                        /*Image = Image.FromFile
-   (System.Environment.GetFolderPath
-   (System.Environment.SpecialFolder.MyPictures)
-   + $"movie_{i+1}.jpg"),*/
-                        Image = (Image)Properties.Resources.ResourceManager.GetObject($"movie_{i + 1}"),
-                        Tag = i + 1,
-                        BorderStyle = BorderStyle.FixedSingle
-                    };
+                pictureBox.Click += PictureBox_Click;
 
-                    pictureBox.Click += PictureBox_Click;
+                this.Controls.Add(pictureBox);
 
-                    this.Controls.Add(pictureBox);
-
-                    // Positioning logic
-                    x += pictureBoxWidth + spacing;
-                    if ((i + 1) % 3 == 0)
-                    {
-                        x = spacing;
-                        y += pictureBoxHeight + spacing;
-                    }
-                }
-                //else
+                // Positioning logic
+                x += pictureBoxWidth + spacing;
+                if ((i + 1) % 3 == 0)
                 {
-                    //MessageBox.Show($"Resource '{resourceName}' not found in Properties.Resources.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    x = spacing;
+                    y += pictureBoxHeight + spacing;
                 }
+
             }
-
         }
 
         private void PictureBox_Click(object sender, EventArgs e)
         {
-            // Get the clicked PictureBox
             PictureBox clickedPictureBox = sender as PictureBox;
 
             if (clickedPictureBox != null)
@@ -120,27 +102,7 @@ namespace Kino.view
                         formOverviewProjections.Show();
                     }
                 }
-
-
-                /*
-                // Access the image or other data (e.g., from the Tag property)
-                int index = (int)clickedPictureBox.Tag;
-                MessageBox.Show($"You clicked on PictureBox with index {index}.", "PictureBox Clicked");
-
-                // Example: Open the image in a new form or perform other actions
-                Form imageForm = new Form
-                {
-                    Size = new Size(800, 600),
-                    BackgroundImage = clickedPictureBox.Image,
-                    BackgroundImageLayout = ImageLayout.Zoom
-                };
-                imageForm.Show();
-                */
-
-
             }
         }
-
-
     }
 }
