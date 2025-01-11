7aea9ba922f5b1e80cbc2ca04511f9c89c9b97a5
MLea1909 <126357233+MLea1909@users.noreply.github.com>
2025-01-11 23:20:13 +0100

    FormEmployers part1

--- Kino/view/FormEmployers.Designer.cs
+++ Kino/view/FormEmployers.Designer.cs
@@ -28,29 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.cinemaDBDataSet = new Kino.CinemaDBDataSet();
+            this.cinemaDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Role = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cinemaDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cinemaDBDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(486, 271);
+            this.label1.Location = new System.Drawing.Point(900, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "employee";
             // 
-            // FormEmployee
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeight = 40;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserID,
+            this.EName,
+            this.Surname,
+            this.Username,
+            this.Role});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1005, 534);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.BackColor = System.Drawing.Color.Transparent;
+            this.labelStatus.ForeColor = System.Drawing.Color.LightCoral;
+            this.labelStatus.Location = new System.Drawing.Point(47, 56);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 16);
+            this.labelStatus.TabIndex = 33;
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.buttonSubmit.Enabled = false;
+            this.buttonSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSubmit.Font = new System.Drawing.Font("Berlin Sans FB Demi", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubmit.ForeColor = System.Drawing.Color.White;
+            this.buttonSubmit.Location = new System.Drawing.Point(849, 625);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(171, 34);
+            this.buttonSubmit.TabIndex = 35;
+            this.buttonSubmit.Text = "SUBMIT CHANGES";
+            this.buttonSubmit.UseVisualStyleBackColor = false;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // cinemaDBDataSet
+            // 
+            this.cinemaDBDataSet.DataSetName = "CinemaDBDataSet";
+            this.cinemaDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cinemaDBDataSetBindingSource
+            // 
+            this.cinemaDBDataSetBindingSource.DataSource = this.cinemaDBDataSet;
+            this.cinemaDBDataSetBindingSource.Position = 0;
+            // 
+            // UserID
+            // 
+            this.UserID.HeaderText = "User ID";
+            this.UserID.MinimumWidth = 6;
+            this.UserID.Name = "UserID";
+            this.UserID.ReadOnly = true;
+            // 
+            // EName
+            // 
+            this.EName.HeaderText = "Name";
+            this.EName.MinimumWidth = 6;
+            this.EName.Name = "EName";
+            this.EName.ReadOnly = true;
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.MinimumWidth = 6;
+            this.Surname.Name = "Surname";
+            this.Surname.ReadOnly = true;
+            // 
+            // Username
+            // 
+            this.Username.HeaderText = "Username";
+            this.Username.MinimumWidth = 6;
+            this.Username.Name = "Username";
+            this.Username.ReadOnly = true;
+            // 
+            // Role
+            // 
+            this.Role.HeaderText = "Role";
+            this.Role.MinimumWidth = 6;
+            this.Role.Name = "Role";
+            this.Role.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Role.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // FormEmployers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Kino.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(1032, 633);
+            this.ClientSize = new System.Drawing.Size(1050, 680);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "FormEmployee";
+            this.Name = "FormEmployers";
             this.Text = "FormEmployee";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cinemaDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cinemaDBDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +178,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelStatus;
+        private CinemaDBDataSet cinemaDBDataSet;
+        private System.Windows.Forms.BindingSource cinemaDBDataSetBindingSource;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Role;
     }
 }
--- Kino/view/FormEmployers.cs
+++ Kino/view/FormEmployers.cs
@@ -1,4 +1,5 @@
 ﻿using Kino.model;
+using Kino.services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,6 +25,80 @@ namespace Kino.view
             Dock = DockStyle.Fill;
             TopLevel = false;
             TopMost = true;
+
+            labelStatus.Text = "";
+
+            FillData();
+        }
+
+        
+        Dictionary<int, string> roles = new Dictionary<int, string>() {
+                                  {1, "Employee"},
+                                  {2, "Admin"}  };
+
+        private void FillData()
+        {
+            UserService userService = new UserService(labelStatus);
+
+            List<User> users = userService.GetUsers();
+
+            var combobox = (DataGridViewComboBoxColumn)dataGridView1.Columns[3];
+            combobox.DataSource = new BindingSource(roles, null);
+            combobox.DisplayMember = "Value";
+            combobox.ValueMember = "Key";
+
+            foreach (User user in users)
+            {
+                dataGridView1.Rows.Add(user.IdUser, user.Name, user.Surname, user.Username, user.Role);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dataGridView1.Columns[e.ColumnIndex].Name == "Role")
+            {
+                // Get the updated value
+                var selectedValue = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+
+                // Perform your logic
+                if (selectedValue != null)
+                {
+                    MessageBox.Show($"Role changed to: {selectedValue}");
+                }
+
+                buttonSubmit.Enabled = true;
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            UserService userService = new UserService(labelStatus);
+
+            List<User> users = userService.GetUsers();
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (!dataGridView1.Rows[i].IsNewRow)
+                {
+                    var userID = dataGridView1.Rows[i].Cells["User ID"].Value?.ToString();
+                    //var username = dataGridView1.Rows[i].Cells["Username"].Value?.ToString();
+                    //Console.WriteLine($"Row {i}: Username = {username}");
+                    //if() ....TO BE CONTINUED
+                }
+            }
+
+
+            foreach (User user in users)
+            {
+                
+            }
+
         }
     }
 }
